62c98a0c78b85e351658272e6a568732f690dfa1
Ashish <78149551+Ashish0209@users.noreply.github.com>
2025-04-08 16:39:31 +0530

    Update test_alerts.py

--- tests/test_alerts.py
+++ tests/test_alerts.py
@@ -4,15 +4,16 @@ from utils.driver_setup import get_lambda_driver
 
 def test_js_alert():
     driver = get_lambda_driver("JS Alert Test")
-    alerts = AlertsPage(driver)
+    alerts = AlertsPage(driver) #Create an instance of AlertsPage
     alerts.handle_js_alert()
+    # Validate the result message
     assert "You successfully clicked an alert" in alerts.get_result()
     driver.quit()
 
 def test_js_confirm():
     driver = get_lambda_driver("JS Confirm Test")
     alerts = AlertsPage(driver)
-    alerts.handle_js_confirm()
+    alerts.handle_js_confirm() #Trigger and accept the JS Confirm dialog
     assert "You clicked: Ok" in alerts.get_result()
     driver.quit()
 
@@ -20,5 +21,6 @@ def test_js_prompt():
     driver = get_lambda_driver("JS Prompt Test")
     alerts = AlertsPage(driver)
     alerts.handle_js_prompt("Ashish")
+    #Assert the entered text
     assert "You entered: Ashish" in alerts.get_result()
     driver.quit()
