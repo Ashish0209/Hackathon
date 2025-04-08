faec592a22fc2dcc4b1bf00b6563bdcd288ebd3e
Ashish <78149551+Ashish0209@users.noreply.github.com>
2025-04-08 16:35:31 +0530

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,12 +4,13 @@ from utils.driver_setup import get_lambda_driver
 from utils.env import USERNAME, PASSWORD
 
 @pytest.mark.parametrize("username,password,expected", [
-    (USERNAME, PASSWORD, "You logged into a secure area!"),
-    ("wronguser", "wrongpass", "Your username is invalid!")
+    (USERNAME, PASSWORD, "You logged into a secure area!"),  # Valid credentials
+    ("wronguser", "wrongpass", "Your username is invalid!")  # Invalid credentials
 ])
 def test_login(username, password, expected):
+    # Initialize
     driver = get_lambda_driver("Login Test")
-    login = LoginPage(driver)
+    login = LoginPage(driver)  #instance of the LoginPage and navigate to the login page
     login.login(username, password)
     assert expected in login.get_message()
     driver.quit()
